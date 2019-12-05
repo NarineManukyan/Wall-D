91625edca822daf5a5ab042c8d5e59e13df0596d
Thomas (TJ) Leach <tjleach98@gmail.com>
2019-12-05 13:57:37 -0500

    Added a comment on what it is and how to use the function.

--- camera.py
+++ camera.py
@@ -1,6 +1,12 @@
 import picamera
 
 def take_picture(file="cameraOutput.png"):
+    """Take a picture and save to disk
+
+        Notes: Default file is 'cameraOutput.png'
+        Params:
+            file = location to save the image
+    """
     camera = picamera.PiCamera()
     camera.start_preview()
     camera.capture(file, format='png')
