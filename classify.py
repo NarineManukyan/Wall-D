d0487b5b99f6b71ff571630966653e808a8bdec8
Finn Jensen <finn.jensen@mymail.champlain.edu>
2019-12-05 13:11:48 -0500

    Main Classify Code
    Cant upload model because its too big.  You can find it here https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels.h5

---
+++ classify.py
@@ -0,0 +1,15 @@
+#REQUIES BOTH KERAS AND IMAGEAI imports
+#https://medium.com/@guymodscientist/image-prediction-with-10-lines-of-code-3266f4039c7a
+from imageai.Prediction import ImagePrediction
+import os
+execution_path = os.getcwd()
+prediction = ImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath(execution_path + "\resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+prediction.loadModel()
+
+if __name__=="__main__":
+    predictions, percentage_probabilities = prediction.predictImage("PASS JPEG?PNG INTO MODEL HERE",
+                                                                    result_count=5)
+    for index in range(len(predictions)):
+        print(predictions[index], " : ", percentage_probabilities[index])
